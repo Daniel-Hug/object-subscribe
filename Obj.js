6ee092db62370bb18d4bf8875e5c0e150c7ae3d2
Daniel Hug <danielthehug@gmail.com>
2014-02-07 10:46:33 -0800

    add cutTies (unsubscribe) function

--- Obj.js
+++ Obj.js
@@ -1,19 +1,25 @@
 // Subscribe to changes on an object:
 var Obj = (function(map) {
-	var Obj = {
-		getSubscribers: function(obj) {
-			for (var i = 0, l = map.length; i < l; i++) {
-				if (map[i][0] === obj) {
-					return map[i][1];
-				}
-			}
-		},
+	function getIndex(obj) {
+		for (var i = 0, l = map.length; i < l; i++) {
+			if (map[i][0] === obj) return i;
+		}
+	}
 
+	return {
 		subscribe: function(obj, fn, callNow) {
-			var subscribers = Obj.getSubscribers(obj);
-			if (subscribers) subscribers.push(fn);
+			var index;
+			if ((index = getIndex(obj))) map[index][1].push(fn);
 			else map.push([obj, [fn]]);
 			if (callNow) fn(obj);
+		},
+
+		cutTies: function(obj, arr) {
+			if (arr) {
+				var subscribers = map[getIndex(obj)][1];
+				for (var l = arr.length; l--;) subscribers.splice(subscribers.indexOf(arr[l]), 1);
+			}
+			else map.splice(getIndex(obj), 1);
 		},
 
 		set: function(obj, key, value) {
@@ -27,12 +33,11 @@ var Obj = (function(map) {
 		},
 
 		changed: function(obj) {
-			var subscribers = Obj.getSubscribers(obj),
+			var subscribers = map[getIndex(obj)][1],
 				numSubscribers = subscribers.length;
 			for (var j = 0; j < numSubscribers; j++) {
 				subscribers[j](obj);
 			}
 		}
 	};
-	return Obj;
 })([]);
