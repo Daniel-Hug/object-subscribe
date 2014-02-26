7abba4e36c7fbf8a75c9a42f5f3365e35b29d38e
Daniel Hug <danielthehug@gmail.com>
2014-02-26 11:36:28 -0800

    Add Obj.keys, Obj.reset, Fix unsubscribe bug

--- Obj.js
+++ Obj.js
@@ -1,5 +1,6 @@
 // Subscribe to changes on an object:
 var Obj = (function(map) {
+	// requires: map
 	function getIndex(obj) {
 		for (var i = 0, l = map.length; i < l; i++) {
 			if (map[i][0] === obj) return i;
@@ -12,38 +13,60 @@ var Obj = (function(map) {
 			return {}.hasOwnProperty.call(obj, key);
 		},
 
+		// requires: has
+		keys: Object.keys || function(obj) {
+			var keys = [];
+			for (var key in obj) if (Obj.has(obj, key)) keys.push(key);
+			return keys;
+		},
+
+		// requires: getIndex, map
 		subscribe: function(obj, fn, callNow) {
 			var index = getIndex(obj);
-			if (index === -1)  map.push([obj, [fn]]);
-			else map[index][1].push(fn);
+			if (index >= 0) map[index][1].push(fn);
+			else map.push([obj, [fn]]);
 			if (callNow) fn(obj);
 		},
 
+		// requires: getIndex, map
 		unsubscribe: function(obj, arr) {
-			if (arr) {
-				var subscribers = map[getIndex(obj)][1];
-				for (var i = arr.length; i--;) subscribers.splice(subscribers.indexOf(arr[i]), 1);
+			var index = getIndex(obj), s;
+			if (index >= 0) {
+				if (arr) {
+					s = map[getIndex(obj)][1];
+					if (s && s.length) for (var i = arr.length; i--;) s.splice(s.indexOf(arr[i]), 1);
+				}
+				else map.splice(getIndex(obj), 1);
 			}
-			else map.splice(getIndex(obj), 1);
 		},
 
+		// requires: has, changed
 		set: function(obj, pairs) {
 			for (var key in pairs) if (Obj.has(pairs, key)) obj[key] = pairs[key];
 			Obj.changed(obj);
 		},
 
+		// requires: changed
 		unset: function(obj, keys) {
 			for (var i = keys.length; i--;) delete obj[keys[i]];
 			Obj.changed(obj);
 		},
 
+		// requires: has, set, changed
+		reset: function(obj, pairs) {
+			for (var key in obj) if (Obj.has(obj, key)) delete obj[key];
+			Obj.set(obj, pairs);
+		},
+
+		// requires: getIndex, map
 		changed: function(obj) {
 			var index = getIndex(obj);
-			if (index === -1) return;
-			var subscribers = map[index][1],
-			numSubscribers = subscribers.length;
-			for (var j = 0; j < numSubscribers; j++) {
-				subscribers[j](obj);
+			if (index >= 0) {
+				var subscribers = map[index][1],
+				numSubscribers = subscribers.length;
+				for (var j = 0; j < numSubscribers; j++) {
+					subscribers[j](obj);
+				}
 			}
 		}
 	};
--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Obj.js
 
 Subscribe to changes on an object
 
-610 bytes minified, 339 bytes minified and gzipped
+800 bytes minified, 401 bytes minified and gzipped
 
 
-## Usage
+## Create & subscribe to an object
 
 Create an object:
 ```
@@ -23,25 +23,55 @@ Obj.subscribe(person, function(newPersonObject) {
 }, true); // Pass true to execute callback now.
 ```
 
+
+## Modify the object
+
 The callback passed to `Obj.subscribe` is called every time one of the following is called:
 
- - `Obj.set` e.g. `Obj.set(person, {name: 'Bob'});`
- - `Obj.unset` e.g. `Obj.unset(person, ['age']);`
- - `Obj.changed` e.g. `person.age++; Obj.changed(person);`
+ - `Obj.set`
+ - `Obj.unset`
+ - `Obj.reset`
+ - `Obj.changed`
 
-Unsubscribe with `Obj.unsubscribe`:
+Use those functions like this:
+```
+Obj.set(person, {age: 11, height: 50, favoriteColor: 'rainbow'});
+	//=> {name: 'Joey', age: 11, height: 50, favoriteColor: 'rainbow'}
+
+Obj.unset(person, ['favoriteColor']);
+	//=> {name: 'Joey', age: 11, height: 50}
+
+Obj.reset(person, {name: 'Bob', age: 6});
+	//=> {name: 'Bob', age: 6}
+
+person.age++;
+Obj.changed(person);
+	//=> {name: 'Bob', age: 7}
+```
+
+
+## Unsubscribe
+
 ```
 Obj.unsubscribe(person); // Unsubscribe all
 Obj.unsubscribe(person, [fn1, fn2]); // Unsubscribe one or more functions
 ```
 
-You also get a bonus `Obj.has` helper function for safe `hasOwnProperty` checks:
+
+## Helper functions
+
+You also get two bonus helper functions. `Obj.has` for safe `hasOwnProperty` checks and `Obj.keys` which returns an array of an object's keys. `Obj.keys` uses the native `Object.keys` if available:
 ```
-Obj.has(person, 'name')
+Obj.has(person, 'name');
+Obj.keys(person);
 ```
 
-## Add your own methods:
 
+## Customize
+
+All the code is very modular, so you can easily delete the parts you don't use.
+
+You can also add your own methods:
 ```
 Obj.increment = function(obj, key) {
 	obj[key]++;
