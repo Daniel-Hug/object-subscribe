fce4c8152a96de74ae01e6012c11e770b5128c0c
Daniel Hug <danielthehug@gmail.com>
2014-03-22 18:45:37 -0700

    Fix export & add tests

--- obj.js
+++ obj.js
@@ -9,7 +9,7 @@
 	else root.Obj = factory();
 })(this, function () {
 	var map = [],
-	objProto = {};
+	Obj = {};
 
 	// requires: map
 	function getIndex(obj) {
@@ -19,88 +19,87 @@
 		return -1;
 	}
 
-	var Obj = {
-		// requires: objProto
-		has: function(obj, key) {
-			return objProto.hasOwnProperty.call(obj, key);
-		},
+	// requires: objProto
+	Obj.has = function(obj, key) {
+		return Obj.hasOwnProperty.call(obj, key);
+	};
 
-		// requires: has
-		keys: Object.keys || function(obj) {
-			var keys = [];
-			for (var key in obj) if (Obj.has(obj, key)) keys.push(key);
-			return keys;
-		},
+	// requires: has
+	Obj.keys = Object.keys || function(obj) {
+		var keys = [];
+		for (var key in obj) if (Obj.has(obj, key)) keys.push(key);
+		return keys;
+	};
 
-		// requires: objProto
-		type: function(obj) {
-			return objProto.toString.call(obj).slice(8,-1).toLowerCase();
-		},
+	// requires: objProto
+	Obj.type = function(obj) {
+		return Obj.toString.call(obj).slice(8,-1).toLowerCase();
+	};
 
-		// requires: has, type, extend
-		extend: function(obj, newObj) {
-			if (typeof obj === 'object') {
-				if (Obj.type(obj) === 'array') {
-					newObj = newObj || [];
-					for (var i = 0, l = obj.length; i < l; i++) newObj.push(Obj.extend(obj[i]));
-				} else {
-					newObj = newObj || {};
-					for (var key in obj) if (Obj.has(obj, key))
-						newObj[key] = Obj.extend(obj[key]);
-				}
-			} else return obj;
-			return newObj;
-		},
-
-		// requires: getIndex, map
-		subscribe: function(obj, fn, callNow) {
-			var mapIndex = getIndex(obj);
-			if (mapIndex >= 0) map[mapIndex][1].push(fn);
-			else map.push([obj, [fn]]);
-			if (callNow) fn(obj);
-		},
-
-		// requires: getIndex, map
-		unsubscribe: function(obj, arr) {
-			var mapIndex = getIndex(obj), s, i;
-			if (mapIndex >= 0) {
-				if (arr) {
-					s = map[mapIndex][1];
-					if (s && s.length) for (i = arr.length; i--;) s.splice(s.indexOf(arr[i]), 1);
-				}
-				else map.splice(mapIndex, 1);
+	// requires: has, type, extend
+	Obj.extend = function(obj, newObj) {
+		if (typeof obj === 'object') {
+			if (Obj.type(obj) === 'array') {
+				newObj = newObj || [];
+				for (var i = 0, l = obj.length; i < l; i++) newObj.push(Obj.extend(obj[i]));
+			} else {
+				newObj = newObj || {};
+				for (var key in obj) if (Obj.has(obj, key))
+					newObj[key] = Obj.extend(obj[key]);
 			}
-		},
+		} else return obj;
+		return newObj;
+	};
 
-		// requires: has, changed
-		set: function(obj, pairs) {
-			for (var key in pairs) if (Obj.has(pairs, key)) obj[key] = pairs[key];
-			Obj.changed(obj);
-		},
+	// requires: getIndex, map
+	Obj.subscribe = function(obj, fn, callNow) {
+		var mapIndex = getIndex(obj);
+		if (mapIndex >= 0) map[mapIndex][1].push(fn);
+		else map.push([obj, [fn]]);
+		if (callNow) fn(obj);
+	};
 
-		// requires: changed
-		unset: function(obj, keys) {
-			for (var i = keys.length; i--;) delete obj[keys[i]];
-			Obj.changed(obj);
-		},
+	// requires: getIndex, map
+	Obj.unsubscribe = function(obj, arr) {
+		var mapIndex = getIndex(obj), s, i;
+		if (mapIndex >= 0) {
+			if (arr) {
+				s = map[mapIndex][1];
+				if (s && s.length) for (i = arr.length; i--;) s.splice(s.indexOf(arr[i]), 1);
+			}
+			else map.splice(mapIndex, 1);
+		}
+	};
 
-		// requires: has, set, changed
-		reset: function(obj, pairs) {
-			for (var key in obj) if (Obj.has(obj, key)) delete obj[key];
-			Obj.set(obj, pairs);
-		},
+	// requires: has, changed
+	Obj.set = function(obj, pairs) {
+		for (var key in pairs) if (Obj.has(pairs, key)) obj[key] = pairs[key];
+		Obj.changed(obj);
+	};
 
-		// requires: getIndex, map
-		changed: function(obj) {
-			var mapIndex = getIndex(obj), i;
-			if (mapIndex >= 0) {
-				var subscribers = map[mapIndex][1],
-				numSubscribers = subscribers.length;
-				for (i = 0; i < numSubscribers; i++) {
-					subscribers[i](obj);
-				}
+	// requires: changed
+	Obj.unset = function(obj, keys) {
+		for (var i = keys.length; i--;) delete obj[keys[i]];
+		Obj.changed(obj);
+	};
+
+	// requires: has, set, changed
+	Obj.reset = function(obj, pairs) {
+		for (var key in obj) if (Obj.has(obj, key)) delete obj[key];
+		Obj.set(obj, pairs);
+	};
+
+	// requires: getIndex, map
+	Obj.changed = function(obj) {
+		var mapIndex = getIndex(obj), i;
+		if (mapIndex >= 0) {
+			var subscribers = map[mapIndex][1],
+			numSubscribers = subscribers.length;
+			for (i = 0; i < numSubscribers; i++) {
+				subscribers[i](obj);
 			}
 		}
 	};
+
 	return Obj;
 });
--- test.js
+++ test.js
@@ -1,2 +1,91 @@
 var Obj = require('./obj.js');
-console.log(Obj.keys(Obj));
+
+var ObjProps = [
+	'has',
+	'keys',
+	'type',
+	'extend',
+	'subscribe',
+	'unsubscribe',
+	'set',
+	'unset',
+	'reset',
+	'changed'
+];
+
+var testObj = {
+	name: 'Daniel',
+	age: 17
+};
+
+
+// has
+console.assert(
+	Obj.has(Obj, 'has'),
+	'Obj.has is not working'
+);
+
+
+// keys
+console.assert(
+	JSON.stringify(Obj.keys(Obj)) === JSON.stringify(ObjProps),
+	'Obj.keys(Obj) returns unexpected value'
+);
+
+
+// type
+console.assert(
+	Obj.type([]) === 'array',
+	'Obj.type([]) is not "array"'
+);
+
+
+// extend
+console.assert(
+	Obj.extend(testObj) !== testObj,
+	'Obj.extend is not working'
+);
+
+
+// subscribe & unsubscribe
+var i = 0;
+Obj.subscribe(testObj, function(newObj) {
+	console.assert(
+		++i === 1 && newObj === testObj,
+	'Obj.subscribe and/or Obj.unsubscribe is not working'
+	);
+}, true);
+
+Obj.unsubscribe(testObj);
+
+
+// set
+Obj.set(testObj, {
+	height: 5
+});
+console.assert(testObj.height === 5, 'Obj.set is not working');
+
+
+// unset
+Obj.unset(testObj, ['height']);
+console.assert(testObj.height === undefined, 'Obj.unset is not working');
+
+
+// reset
+Obj.reset(testObj, {color: 'red'});
+console.assert(
+	JSON.stringify(testObj) === JSON.stringify({color: 'red'}),
+	'Obj.reset is not working'
+);
+
+
+// changed
+Obj.subscribe(testObj, function(newObj) {
+	console.assert(
+		newObj.color === 'green',
+		'Obj.changed is not working'
+	);
+});
+
+testObj.color = 'green';
+Obj.changed(testObj);
